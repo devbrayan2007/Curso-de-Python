0c89567fe4a6a0f1618795948b6a76c00275884d
devbrayan2007 <devbrayan2007@gmail.com>
2024-11-26 13:31:19 -0300

    update

---
+++ secao01/aula007.py
@@ -0,0 +1,10 @@
+# VARIÁVEIS
+
+
+nome_completo = "Brayan Campos"
+idade = 17
+maior_de_idade = idade >= 18 
+print("Nome:", nome_completo)
+print("Idade:", idade)
+print("É maior?", maior_de_idade)
+numero = int(1)
